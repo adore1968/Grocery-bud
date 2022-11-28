e538d38affc21782de7c834b8172d5b75ad38af8
José Gómez <jg345762@gmail.com>
2022-11-28 18:26:47 -0300

    Updated background color

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import List from "./components/List";
 function App() {
   const { alert, list } = useGlobalContext();
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-gray-800">
       <section className="min-h-screen px-5 sm:px-8 py-5 sm:py-8 flex justify-center items-center">
         <div className="bg-white p-5 rounded shadow-white container sm:max-w-2xl">
           {alert.show ? <Alert alert={alert} /> : null}
